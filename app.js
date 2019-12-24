54962f0af4a93a9e611424e990d2b2262994961c
Jonathan Palacio <jonathan@pop-os.localdomain>
2019-12-24 13:31:38 -0500

    Added 2 routes... one for /api/places/:pid and one for /api/places/user/:uid

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const placesRoutes = require('./routes/places-routes');
+//const userRoutes = require('./routes/user-routes');
 
 const app = express();
 
-app.use(placesRoutes);
+app.use('/api/places', placesRoutes);
+//app.use('/api/user', userRoutes);
 
 app.listen(5000);
