3d1f788f18984fd9526313a1bd384dd8f8e20a31
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-12-31 13:18:30 -0500

    added ability to login with mongoose... still working

--- controllers/users-controllers.js
+++ controllers/users-controllers.js
@@ -56,12 +56,22 @@ const signup = async (req, res, next) => {
 
   res.status(201).json({ user: createdUser.toObject({ getters: true }) });
 };
-const login = (req, res, next) => {
+
+const login = async (req, res, next) => {
   const { email, password } = req.body;
-  const identifiedUser = DUMMY_USERS.find(u => u.email === email);
-  if (!identifiedUser || identifiedUser.password !== password) {
-    throw new HttpError("Could not identify user", 401);
+
+  try {
+    existingUser = await User.findOne({ email: email });
+  } catch (err) {
+    const error = new HttpError("login failed", 500);
+    return next(error);
   }
+
+  if (!existingUser || existingUser.password !== password) {
+    const error = new HttpError("Invalid creds", 401);
+    return next(error);
+  }
+
   res.json({ message: "Logged in" });
 };
 
